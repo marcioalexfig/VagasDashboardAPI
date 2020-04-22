fe76c9714a5a4d1881119cc0fee7e4f143184440
Márcio Alex <alex.marcio@gmail.com>
2020-04-22 11:19:46 -0300

    Criação de projeto Spring Boot com Java 14

---
+++ src/main/java/br/com/marcioalexfig/vagasdashboard/VagasDashboardBackApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcioalexfig.vagasdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VagasDashboardBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VagasDashboardBackApplication.class, args);
+	}
+
+}
